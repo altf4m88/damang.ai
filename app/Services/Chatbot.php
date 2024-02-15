31b9459d9105ed60bb779de774443a402b2f173a
fadhilmauladhani <fadhilmauladhani@gmail.com>
2024-02-15 23:51:24 +0700

    modify route prefix and chatbot instruction

--- app/Services/Chatbot.php
+++ app/Services/Chatbot.php
@@ -2,6 +2,9 @@
 
 namespace App\Services;
 
+use App\Models\MedicalRecord;
+use App\Models\User;
+use Carbon\Carbon;
 use OpenAI;
 
 class Chatbot
@@ -11,8 +14,9 @@ class Chatbot
     private $apiToken;
     private $config;
 
-    public function __construct()
+    public function __construct($userId)
     {
+        $this->setInitialMessage($userId);
         $this->apiToken = env('OPENAI_API_KEY');
         $this->config = [
             "model" => "gpt-3.5-turbo",
@@ -22,18 +26,35 @@ class Chatbot
             "frequency_penalty" => 0.1,
             "presence_penalty" => 0
         ];
+        
+
+        $this->client = OpenAI::client($this->apiToken);
+    }
+
+    private function setInitialMessage($userId)
+    {
+        $user = User::findOrFail($userId);
+        $medicalRecord = MedicalRecord::where('user_id', $userId)->first();
+        $age = Carbon::parse($user->date_of_birth)->diffByYear(Carbon::now());
+
         $this->initialMessage = [
             [
                 "role" => "system",
-                "content" => "You are a Medical Consultant called Damang.ai. Use the following principles in responding to users:\n    \n- You only answer medical-related questions.\n- You refuse to answer non-medical-related questions. \n-You don't have to give any advice to non-medical-related questions.\n- Based on this data, you will give personalized health consultation: the user's age is 25, gender is male,  weight is 56kg and height is 170cm. \n- Demonstrate humility by acknowledging your limitations and uncertainties.\n- Be cheerful and give positive vibes to the users.\n- You must answer in Bahasa Indonesia."
+                "content" => "You are a Medical Consultant called Damang.ai. Use the following principles in responding to users:\n    \n
+                - You only answer medical-related questions.\n
+                - You refuse to answer non-medical-related questions. \n
+                - You don't have to give any advice to non-medical-related questions.\
+                - Based on this data, you will give personalized health consultation: the user's age is $age, gender is $user->gender,  weight is $medicalRecord->weight kg and height is $medicalRecord->height cm. \n
+                - User allergies are : $medicalRecord->allergies.\n
+                - Demonstrate humility by acknowledging your limitations and uncertainties.\n
+                - Be cheerful and give positive vibes to the users.\n
+                - You must answer in Bahasa Indonesia."
             ],
             [
                 "role" => "assistant",
                 "content" => "Halo! Saya Damang.ai, Konsultan Medis yang siap membantu Anda. Silakan ajukan pertanyaan medis Anda, dan saya akan dengan senang hati menjawabnya. Ingat, saya hanya dapat menjawab pertanyaan yang berhubungan dengan kesehatan. Mari kita mulai!"
             ]
         ];
-
-        $this->client = OpenAI::client($this->apiToken);
     }
 
     public function sendRequest($message)
